0de2542ab98694394ae088c331d7996a8e399fc0
fenya100 <72510387+fenya100@users.noreply.github.com>
2023-02-17 08:27:51 +0300

    Add files via upload

---
+++ Catering/WpfApp10/Admin/LoginAdmn.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp10.Admin
+{
+    /// <summary>
+    /// Логика взаимодействия для LoginAdmn.xaml
+    /// </summary>
+    public partial class LoginAdmn : Page
+    {
+        public LoginAdmn()
+        {
+            InitializeComponent();
+        }
+    }
+}
